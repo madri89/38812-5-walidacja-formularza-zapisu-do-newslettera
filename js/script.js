9f5408c3dac92755b71efe10bf1fd7c14ca7bb74
madri89 <marcinrossa@wp.pl>
2022-07-27 23:44:22 +0200

    version1

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let newsletterForm = document.getElementById('newsletter-form');
+let allAgreeChx = document.getElementById('all-agree');
+
+const validate = (event) => {
+
+    let txtName = document.getElementById('name');
+    let txtEmail = document.getElementById('email');
+    let agree1 = document.getElementById('agree-1');
+    let errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    if (txtName.value.trim() === ''){
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz Imie i Nazwisko';
+        errors.appendChild(liError);
+    };
+
+    if (txtEmail.value.trim() === ''){
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz adres e-mail';
+        errors.appendChild(liError);
+    };
+
+    if (!txtEmail.value.includes('@')){
+        let liError = document.createElement('li');
+        liError.innerText = 'Adres e-mail nie ma znaku @';
+        errors.appendChild(liError);
+    };
+
+    if (!agree1.checked) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Brak zgody 1';
+        errors.appendChild(liError);
+    };
+
+    if (errors.children.length > 0){
+        event.preventDefault();
+    };
+    
+    
+};
+
+const allAgree = (event) => {
+    let agree1 = document.getElementById('agree-1');
+    let agree2 = document.getElementById('agree-2');
+
+    agree1.checked = event.target.checked;
+    agree2.checked = event.target.checked;
+
+    agree1.disabled = event.target.checked;
+    agree2.disabled = event.target.checked;
+    // console.log(event.target.checked);
+};
+
+
+allAgreeChx.addEventListener('change', allAgree);
+newsletterForm.addEventListener('submit', validate);
